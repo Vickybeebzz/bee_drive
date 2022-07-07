89eb11bdab0158b7e54972dea708733f01f18244
Jose María Arias Gallego-Acho <sr.conejo@icloud.com>
2022-07-07 17:33:52 +0200

    Refactor code

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+let isBeeDriving = false;
+
 function getDistance() {
   const distance = localStorage.getItem("distance");
   if (distance == null) {
@@ -17,15 +19,17 @@ function drive() {
     "distance",
     getDistance() + Math.floor(Math.random() * 100)
   );
+  isBeeDriving = true;
   changeImage();
   setTimeout(writeText, 2000);
+  setTimeout((isBeeDriving = false), 2000);
   setTimeout(changeImage, 2000);
 }
 
 function changeImage() {
   const image = document.getElementById("car");
 
-  if (image.src === "./images/beecar.png") {
+  if (isBeeDriving == true) {
     image.src = "./images/drive.gif";
   } else {
     image.src = "./images/beecar.png";
