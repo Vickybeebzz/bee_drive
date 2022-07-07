1a0bd1158d2ba68c3162f6b7f8d4242006ad78fd
Jose María Arias Gallego-Acho <sr.conejo@icloud.com>
2022-07-07 14:40:26 +0200

    Fix Init

--- app.js
+++ app.js
@@ -1,27 +1,31 @@
 let distance = 0;
 
-function writeText(){
-  document.getElementById("textbox").innerHTML = `Little Bee Beelioli has driven ${distance} km.`;
+function writeText() {
+  document.getElementById(
+    "textbox"
+  ).innerHTML = `Little Bee Beelioli has driven ${distance} km.`;
 }
 
-function drive(){
-  distance = parseInt(localStorage.getItem("distance")) + Math.floor(Math.random()*100);
-  localStorage.setItem("distance",distance);
+function drive() {
+  distance =
+    parseInt(localStorage.getItem("distance")) +
+    Math.floor(Math.random() * 100);
+  localStorage.setItem("distance", distance);
   writeText();
 }
 
-function reset(){
-  localStorage.setItem("distance",0);
+function reset() {
+  localStorage.setItem("distance", 0);
   distance = parseInt(localStorage.getItem("distance"));
   writeText();
 }
 
-if(parseInt(localStorage.getItem("distance") < 0)){
-localStorage.setItem("distance",0);
-};
+if (localStorage.getItem("distance") == null) {
+  localStorage.setItem("distance", 0);
+}
 
 distance = parseInt(localStorage.getItem("distance"));
 
 writeText();
-document.getElementById("btn-drive").addEventListener("click",drive);
-document.getElementById("btn-reset").addEventListener("click",reset);
+document.getElementById("btn-drive").addEventListener("click", drive);
+document.getElementById("btn-reset").addEventListener("click", reset);
