0ef7be7348744c221e5b4e1c572585f36283337c
Jose María Arias Gallego-Acho <sr.conejo@icloud.com>
2022-07-08 20:39:13 +0200

    Add object driver

--- app.js
+++ app.js
@@ -1,4 +1,24 @@
 let isBeeDriving = false;
+let driver = new BeeDriver();
+
+function BeeDriver() {
+  this.isDriving = false;
+
+  this.drive = function () {
+    this.isDriving = true;
+    console.log(this.isDriving);
+    changeImage();
+    setTimeout(() => {
+      localStorage.setItem(
+        "distance",
+        getDistance() + Math.floor(Math.random() * 100)
+      );
+      writeText();
+      () => (this.isDriving = false);
+      changeImage();
+    }, 2000);
+  };
+}
 
 function getDistance() {
   const distance = localStorage.getItem("distance");
@@ -14,24 +34,25 @@ function writeText() {
   ).innerHTML = `Little Bee Beelioli has driven ${getDistance()} km.`;
 }
 
-function drive() {
-  localStorage.setItem(
-    "distance",
-    getDistance() + Math.floor(Math.random() * 100)
-  );
-  isBeeDriving = true;
-  changeImage();
-  setTimeout(writeText, 2000);
-  setTimeout((isBeeDriving = false), 2000);
-  setTimeout(changeImage, 2000);
-}
+//function drive() {
+//  localStorage.setItem(
+//    "distance",
+//    getDistance() + Math.floor(Math.random() * 100)
+//  );
+//  isBeeDriving = true;
+//  changeImage();
+//  setTimeout(writeText, 2000);
+//  setTimeout((isBeeDriving = false), 2000);
+//  setTimeout(changeImage, 2000);
+//}
 
 function changeImage() {
   const image = document.getElementById("car");
-
-  if (isBeeDriving == true) {
+  if (driver.isDriving == true) {
+    console.log(this.isDriving);
     image.src = "./images/drive.gif";
   } else {
+    console.log(this.isDriving);
     image.src = "./images/beecar.png";
   }
 }
@@ -42,5 +63,5 @@ function reset() {
 }
 
 writeText();
-document.getElementById("btn-drive").addEventListener("click", drive);
+document.getElementById("btn-drive").addEventListener("click", driver.drive);
 document.getElementById("btn-reset").addEventListener("click", reset);
